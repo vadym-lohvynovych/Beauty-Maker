7bcd5e1ccd0ffd8159b8703170600a7ca944db3e
unknown <vadvoker@gmail.com>
2018-12-24 20:33:32 +0200

    optimization

--- src/js/main.js
+++ src/js/main.js
@@ -1,60 +1,75 @@
 $(document).ready(function() {
 
-    let defaultOffsets = {
-        missionTitle: $('.mission-title').offset().top,
-        projectsTitle: $('.projects-title').offset().top,
-        teamTitle: $('.team-title').offset().top,
-        eventTitle: $('.event-title').offset().top,
-        projectsLetter: $('.projects-letter').offset().top,
-        eventLetter: $('.event-letter').offset().top,
-        missionImg: $('.mission-img').offset().top,
-        teamItem1Img: $('.team-item-1 .team-item-img').offset().top,
-        teamItem2Img: $('.team-item-2 .team-item-img').offset().top,
-        teamItem3Img: $('.team-item-3 .team-item-img').offset().top,
-        teamItem4Img: $('.team-item-4 .team-item-img').offset().top,
-        teamItem5Img: $('.team-item-5 .team-item-img').offset().top
-
+    const selectors = {
+        teamItem1Text: $('.team-item-1 .team-item-text'),
+        teamItem2Text: $('.team-item-2 .team-item-text'),
+        teamItem3Text: $('.team-item-3 .team-item-text'),
+        teamItem4Text: $('.team-item-4 .team-item-text'),
+        teamItem5Text: $('.team-item-5 .team-item-text'),
+        teamItem1Img: $('.team-item-1 .team-item-img'),
+        teamItem2Img: $('.team-item-2 .team-item-img'),
+        teamItem3Img: $('.team-item-3 .team-item-img'),
+        teamItem4Img: $('.team-item-4 .team-item-img'),
+        teamItem5Img: $('.team-item-5 .team-item-img'),
+        eventDescr: $('.event-description'),
+        missionTitle: $('.mission-title'),
+        projectsTitle: $('.projects-title'),
+        teamTitle: $('.team-title'),
+        eventTitle: $('.event-title'),
+        projectsLetter: $('.projects-letter'),
+        teamLetter: $('.team-letter'),
+        eventLetter: $('.event-letter'),
+        missionImg: $('.mission-img'),
     };
+
+    let defaultOffsets = {
+        missionTitle: selectors.missionTitle.offset().top,
+        projectsTitle: selectors.projectsTitle.offset().top,
+        teamTitle: selectors.teamTitle.offset().top,
+        eventTitle: selectors.eventTitle.offset().top,
+        projectsLetter: selectors.projectsLetter.offset().top,
+        eventLetter: selectors.eventLetter.offset().top,
+        teamLetter: selectors.teamLetter.offset().top,
+        missionImg: selectors.missionImg.offset().top,
+        teamItem1Img: selectors.teamItem1Img.offset().top,
+        teamItem2Img: selectors.teamItem2Img.offset().top,
+        teamItem3Img: selectors.teamItem3Img.offset().top,
+        teamItem4Img: selectors.teamItem4Img.offset().top,
+        teamItem5Img: selectors.teamItem5Img.offset().top
+    };
+    
+    let st = 0,
+        wh = $(window).height();
 
     $('.header-text').css('opacity', 1);
     
     $(window).on('scroll', function() {
-        showTeamItemHeader('.team-item-1 .team-item-text');
-        showTeamItemHeader('.team-item-2 .team-item-text');
-        showTeamItemHeader('.team-item-3 .team-item-text');
-        showTeamItemHeader('.team-item-4 .team-item-text');
-        showTeamItemHeader('.team-item-5 .team-item-text');
-        showTeamItemHeader('.event-description', 120);
+        
+        st = $(window).scrollTop();
 
-        moveHeader();
-        moveAndAnimateSectionTitleOnScroll('.mission-title', defaultOffsets.missionTitle, .7);
-        moveAndAnimateSectionTitleOnScroll('.projects-title', defaultOffsets.projectsTitle, .5);
-        moveAndAnimateSectionTitleOnScroll('.team-title', defaultOffsets.teamTitle, .3);
-        moveAndAnimateSectionTitleOnScroll('.event-title', defaultOffsets.eventTitle, .8);
+        moveAndAnimateSectionTitleOnScroll(selectors.missionTitle, defaultOffsets.missionTitle, .7);
+        moveAndAnimateSectionTitleOnScroll(selectors.projectsTitle, defaultOffsets.projectsTitle, .5);
+        moveAndAnimateSectionTitleOnScroll(selectors.teamTitle, defaultOffsets.teamTitle, .3);
+        moveAndAnimateSectionTitleOnScroll(selectors.eventTitle, defaultOffsets.eventTitle, .8);
 
-        moveItemOnScroll('.mission-img', defaultOffsets.missionImg, .15);
-        moveItemOnScroll('.team-item-1 .team-item-img', defaultOffsets.teamItem1Img, .1);
-        moveItemOnScroll('.team-item-2 .team-item-img', defaultOffsets.teamItem2Img, .2);
-        moveItemOnScroll('.team-item-3 .team-item-img', defaultOffsets.teamItem3Img, -.13);
-        moveItemOnScroll('.team-item-4 .team-item-img', defaultOffsets.teamItem4Img, .2);
-        moveItemOnScroll('.team-item-5 .team-item-img', defaultOffsets.teamItem5Img, -.2);
+        moveItemOnScroll(selectors.missionImg, defaultOffsets.missionImg, .15);
+        moveItemOnScroll(selectors.teamItem1Img, defaultOffsets.teamItem1Img, .1);
+        moveItemOnScroll(selectors.teamItem2Img, defaultOffsets.teamItem2Img, .2);
+        moveItemOnScroll(selectors.teamItem3Img, defaultOffsets.teamItem3Img, -.13);
+        moveItemOnScroll(selectors.teamItem4Img, defaultOffsets.teamItem4Img, .2);
+        moveItemOnScroll(selectors.teamItem5Img, defaultOffsets.teamItem5Img, -.2);
 
-        moveItemOnScroll('.projects-letter', defaultOffsets.projectsLetter, .2);
-        moveItemOnScroll('.team-letter', defaultOffsets.projectsLetter, .26);
-        moveItemOnScroll('.event-letter', defaultOffsets.eventLetter, -.2);
+        moveItemOnScroll(selectors.projectsLetter, defaultOffsets.projectsLetter, .2);
+        moveItemOnScroll(selectors.teamLetter, defaultOffsets.teamLetter, .26);
+        moveItemOnScroll(selectors.eventLetter, defaultOffsets.eventLetter, -.2);
+
+
+    }).on('resize', function() {
+        wh = $(window).height();
     });
 
-    animateTextShowing('.header-text', 40);
 
-    
-    function showTeamItemHeader(selector, interval) {
-        if($(window).scrollTop() + $(window).height() > $(selector).offset().top) {
-            if(!$(selector).hasClass('visible')) {
-                $(selector).addClass('visible');
-                animateTextShowing($(selector).find('h3'), (interval || 50));
-            }
-        }
-    }
+    animateTextShowing('.header-text', 40);
 
     function animateTextShowing(selector, interval) {
         
@@ -89,30 +104,21 @@ $(document).ready(function() {
         }, 500)
     }
 
-    function moveHeader() {
-        if($(window).scrollTop() < 500) {
-            $('header').css({
-                'transform': 'translateY(-' + $(window).scrollTop() * .5 + 'px)',
-                'opacity': 1 - $(window).scrollTop() / 300
-            });
-        }
-    }
-
     function moveAndAnimateSectionTitleOnScroll(selector, defaultOffset, speedCoef) {
         //if block is on screen
-        if(($(window).scrollTop() + $(window).height()) > $(selector).offset().top && $(window).scrollTop() < $(selector).offset().top + $(selector).height()) {
-            $(selector).css('transform', 'translateY(-'+ (($(window).scrollTop() + $(window).height() - defaultOffset) * (speedCoef || 1)) + 'px)');
-        }
-        if(!$(selector).hasClass('showed') ) {
-            $(selector).addClass('showed');
-            animateTextShowing($(selector).find('p:nth-child(2)'), 200);
+        if((st + wh) > selector.offset().top && st < selector.offset().top + selector.height()) {
+            selector.css('transform', 'translateY(-'+ ((st + wh - defaultOffset) * (speedCoef || 1)) + 'px)');
+            if(!selector.hasClass('showed') ) {
+                selector.addClass('showed');
+                animateTextShowing(selector.find('p:nth-child(2)'), 200);
+            }
         }
     }
 
     function moveItemOnScroll(selector, defaultOffset, speedCoef) {
         //if block is on screen
-        if(($(window).scrollTop() + $(window).height()) > $(selector).offset().top && $(window).scrollTop() < $(selector).offset().top + $(selector).height()) {
-            $(selector).css('transform', 'translateY('+ ((($(window).scrollTop()) + $(window).height() - defaultOffset  + 300) * (speedCoef || 1)) + 'px)');
+        if((st + wh) > selector.offset().top && st < selector.offset().top + selector.height()) {
+            $(selector).css('transform', 'translateY('+ (((st) + wh - defaultOffset  + 300) * (speedCoef || 1)) + 'px)');
         }
     }
 
